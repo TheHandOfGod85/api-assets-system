6f5fdb851800237c0716f81651333ebd648533ca
Daniele Del Piano <daniele.delpiano30@gmail.com>
2024-06-06 17:11:10 +0100

    get current user implemented

---
+++ Application/Identity/CurrentAppUser.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+
+namespace Application;
+
+public class CurrentAppUser
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public CurrentAppUser(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+    public Guid AppUserId => Guid.Parse(
+        _httpContextAccessor.HttpContext?.User
+        .FindFirstValue("AppUserId") ?? default(Guid).ToString()
+    );
+}
